f46549d63f7c45725bde8e5d773ed2d09a4d416f
F0x1d <maks.zrandles@gmail.com>
2021-02-28 11:24:50 +0500

    Beautify response with hacker font

--- src/main/java/com/f0x1d/foxbin/restcontroller/note/NoteController.java
+++ src/main/java/com/f0x1d/foxbin/restcontroller/note/NoteController.java
@@ -1,50 +1,24 @@
 package com.f0x1d.foxbin.restcontroller.note;
 
-import com.f0x1d.foxbin.model.requestbody.CreateOrEditNoteRequestBody;
-import com.f0x1d.foxbin.model.response.base.Response;
-import com.f0x1d.foxbin.model.response.note.CreatedOrEditedNoteResponse;
-import com.f0x1d.foxbin.model.response.note.UserNoteResponse;
-import com.f0x1d.foxbin.model.response.note.UserNotesResponse;
 import com.f0x1d.foxbin.service.NoteService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
-@RestController
+@Controller
 public class NoteController {
 
     @Autowired
     private NoteService mNoteService;
 
-    @PostMapping("/create")
-    public Response createNote(@RequestBody CreateOrEditNoteRequestBody createOrEditNoteRequestBody) {
-        return new CreatedOrEditedNoteResponse(mNoteService.createNote(
-                createOrEditNoteRequestBody.getContent(),
-                createOrEditNoteRequestBody.getSlug(),
-                createOrEditNoteRequestBody.getAccessToken()
-        ));
-    }
-
-    @PostMapping("/edit")
-    public Response editNote(@RequestBody CreateOrEditNoteRequestBody createOrEditNoteRequestBody) {
-        return new CreatedOrEditedNoteResponse(mNoteService.editNote(
-                createOrEditNoteRequestBody.getContent(),
-                createOrEditNoteRequestBody.getSlug(),
-                createOrEditNoteRequestBody.getAccessToken()
-        ));
-    }
-
     @GetMapping("/{slug}")
-    public String rawNote(@PathVariable String slug) {
-        return mNoteService.getRawNote(slug);
-    }
+    public String rawNote(@PathVariable String slug, Model model) {
+        String text = mNoteService.getRawNote(slug);
 
-    @GetMapping("/getAll")
-    public Response getNotes(@RequestParam(value = "accessToken") String accessToken) {
-        return new UserNotesResponse(mNoteService.userNotes(accessToken));
-    }
-
-    @GetMapping("/get/{slug}")
-    public Response getNote(@PathVariable String slug, @RequestParam(value = "accessToken", required = false) String accessToken) {
-        return new UserNoteResponse(mNoteService.getNote(slug, accessToken));
+        model.addAttribute("slug", slug);
+        model.addAttribute("text", text);
+        return "note";
     }
 }
---
+++ src/main/java/com/f0x1d/foxbin/restcontroller/note/NoteRestController.java
@@ -0,0 +1,46 @@
+package com.f0x1d.foxbin.restcontroller.note;
+
+import com.f0x1d.foxbin.model.requestbody.CreateOrEditNoteRequestBody;
+import com.f0x1d.foxbin.model.response.base.Response;
+import com.f0x1d.foxbin.model.response.note.CreatedOrEditedNoteResponse;
+import com.f0x1d.foxbin.model.response.note.UserNoteResponse;
+import com.f0x1d.foxbin.model.response.note.UserNotesResponse;
+import com.f0x1d.foxbin.service.NoteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class NoteRestController {
+
+    @Autowired
+    private NoteService mNoteService;
+
+    @PostMapping("/create")
+    public Response createNote(@RequestBody CreateOrEditNoteRequestBody createOrEditNoteRequestBody) {
+        return new CreatedOrEditedNoteResponse(mNoteService.createNote(
+                createOrEditNoteRequestBody.getContent(),
+                createOrEditNoteRequestBody.getSlug(),
+                createOrEditNoteRequestBody.getAccessToken()
+        ));
+    }
+
+    @PostMapping("/edit")
+    public Response editNote(@RequestBody CreateOrEditNoteRequestBody createOrEditNoteRequestBody) {
+        return new CreatedOrEditedNoteResponse(mNoteService.editNote(
+                createOrEditNoteRequestBody.getContent(),
+                createOrEditNoteRequestBody.getSlug(),
+                createOrEditNoteRequestBody.getAccessToken()
+        ));
+    }
+
+    @GetMapping("/getAll")
+    public Response getNotes(@RequestParam(value = "accessToken") String accessToken) {
+        return new UserNotesResponse(mNoteService.userNotes(accessToken));
+    }
+
+    @GetMapping("/get/{slug}")
+    public Response getNote(@PathVariable String slug, @RequestParam(value = "accessToken", required = false) String accessToken) {
+        return new UserNoteResponse(mNoteService.getNote(slug, accessToken));
+    }
+}
