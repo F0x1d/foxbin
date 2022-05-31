6615b97dce877a927f0af421d2cfd1c2614ec57d
F0x1d <maks.zrandles@gmail.com>
2022-05-31 18:27:17 +0500

    Database transactions

--- src/main/java/com/f0x1d/foxbin/repository/NoteRepository.java
+++ src/main/java/com/f0x1d/foxbin/repository/NoteRepository.java
@@ -1,6 +1,7 @@
 package com.f0x1d.foxbin.repository;
 
 import com.f0x1d.foxbin.Constants;
+import com.f0x1d.foxbin.database.ObjectBox;
 import com.f0x1d.foxbin.database.model.FoxBinNote;
 import com.f0x1d.foxbin.database.model.FoxBinNote_;
 import com.f0x1d.foxbin.database.model.FoxBinUser;
@@ -11,6 +12,7 @@ import io.objectbox.query.QueryBuilder;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
@@ -18,69 +20,75 @@ public class NoteRepository {
 
     private FoxBinUser mRootUser;
 
-    public List<FoxBinNote> getAll() {
-        return DBUtils.boxForNotes().getAll();
-    }
-
     public void createNote(String content, String slug, long deleteAfter, FoxBinUser user) {
-        DBUtils
+        ObjectBox.get().runInTx(() -> DBUtils
                 .boxForNotes()
                 .put(FoxBinNote.createNote(
                         content,
                         slug,
                         deleteAfter,
                         user == null ? rootUser() : user
-                ));
+                ))
+        );
     }
 
     public void deleteNote(FoxBinNote foxBinNote) {
-        DBUtils
+        ObjectBox.get().runInTx(() -> DBUtils
                 .boxForNotes()
-                .remove(foxBinNote);
+                .remove(foxBinNote)
+        );
     }
 
     public void editNote(String content, FoxBinNote foxBinNote) {
-        foxBinNote.setContent(content);
+        ObjectBox.get().runInTx(() -> {
+            foxBinNote.setContent(content);
 
-        DBUtils
-                .boxForNotes()
-                .put(foxBinNote);
+            DBUtils
+                    .boxForNotes()
+                    .put(foxBinNote);
+        });
     }
 
     public FoxBinNote noteFromSlug(String slug) {
-        return DBUtils.findFirst(
+        return ObjectBox.get().callInReadTx(() -> DBUtils.findFirst(
                 DBUtils
                         .boxForNotes()
                         .query()
                         .equal(FoxBinNote_.slug, slug, QueryBuilder.StringOrder.CASE_SENSITIVE)
                         .build()
-        );
+        ));
     }
 
     private FoxBinUser rootUser() {
-        return mRootUser == null ? mRootUser = DBUtils.findFirst(
+        return ObjectBox.get().callInReadTx(() -> mRootUser == null ? mRootUser = DBUtils.findFirst(
                 DBUtils
                         .boxForUsers()
                         .query()
                         .equal(FoxBinUser_.username, "root", QueryBuilder.StringOrder.CASE_SENSITIVE)
                         .build()
-        ) : mRootUser;
+        ) : mRootUser);
     }
 
     @Scheduled(initialDelay = Constants.ONE_MINUTE_MS, fixedRate = Constants.THREE_MINUTES_MS)
     public void checkExpiration() {
-        Box<FoxBinNote> noteBox = DBUtils.boxForNotes();
+        ObjectBox.get().runInTx(() -> {
+            Box<FoxBinNote> noteBox = DBUtils.boxForNotes();
 
-        List<FoxBinNote> expiringNotes = DBUtils.find(
-                noteBox
-                        .query()
-                        .greater(FoxBinNote_.deleteAfter, 0)
-                        .build()
-        );
+            List<FoxBinNote> expiringNotes = DBUtils.find(
+                    noteBox
+                            .query()
+                            .greater(FoxBinNote_.deleteAfter, 0)
+                            .build()
+            );
 
-        for (FoxBinNote expiringNote : expiringNotes) {
-            if (expiringNote.getDate().getTime() + expiringNote.getDeleteAfter() <= System.currentTimeMillis())
-                noteBox.remove(expiringNote);
-        }
+            List<FoxBinNote> notesToRemove = new ArrayList<>();
+
+            for (FoxBinNote expiringNote : expiringNotes) {
+                if (expiringNote.getDate().getTime() + expiringNote.getDeleteAfter() <= System.currentTimeMillis())
+                    notesToRemove.add(expiringNote);
+            }
+
+            noteBox.remove(notesToRemove);
+        });
     }
 }
--- src/main/java/com/f0x1d/foxbin/repository/UserRepository.java
+++ src/main/java/com/f0x1d/foxbin/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.f0x1d.foxbin.repository;
 
 import com.f0x1d.foxbin.Constants;
+import com.f0x1d.foxbin.database.ObjectBox;
 import com.f0x1d.foxbin.database.model.AccessToken;
 import com.f0x1d.foxbin.database.model.AccessToken_;
 import com.f0x1d.foxbin.database.model.FoxBinUser;
@@ -9,90 +10,100 @@ import com.f0x1d.foxbin.restcontroller.user.exceptions.InvalidAuthorizationExcep
 import com.f0x1d.foxbin.utils.CryptUtils;
 import com.f0x1d.foxbin.utils.DBUtils;
 import io.objectbox.Box;
-import io.objectbox.exception.UniqueViolationException;
 import io.objectbox.query.QueryBuilder;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
 public class UserRepository {
 
     public List<FoxBinUser> getAllUsers() {
-        return DBUtils.boxForUsers().getAll();
+        return ObjectBox.get().callInReadTx(() -> DBUtils.boxForUsers().getAll());
     }
 
     public FoxBinUser foxBinUserByUsername(String username) {
-        return DBUtils.findFirst(
+        return ObjectBox.get().callInReadTx(() -> DBUtils.findFirst(
                 DBUtils
                         .boxForUsers()
                         .query()
                         .equal(FoxBinUser_.username, username, QueryBuilder.StringOrder.CASE_SENSITIVE)
                         .build()
-        );
+        ));
     }
 
     public FoxBinUser userFromAccessToken(String token) {
-        Box<AccessToken> accessTokenBox = DBUtils.boxForTokens();
+        return ObjectBox.get().callInTxNoException(() -> {
+            Box<AccessToken> accessTokenBox = DBUtils.boxForTokens();
 
-        AccessToken accessToken = DBUtils.findFirst(
-                accessTokenBox
-                        .query()
-                        .equal(AccessToken_.token, token, QueryBuilder.StringOrder.CASE_SENSITIVE)
-                        .build()
-        );
+            AccessToken accessToken = DBUtils.findFirst(
+                    accessTokenBox
+                            .query()
+                            .equal(AccessToken_.token, token, QueryBuilder.StringOrder.CASE_SENSITIVE)
+                            .build()
+            );
 
-        if (accessToken == null)
-            throw new InvalidAuthorizationException();
+            if (accessToken == null)
+                throw new InvalidAuthorizationException();
 
-        accessTokenBox.put(accessToken.setLastUsedNow());
-        return accessToken.getUser().getTarget();
+            accessTokenBox.put(accessToken.setLastUsedNow());
+            return accessToken.getUser().getTarget();
+        });
     }
 
-    public void addAccessToken(FoxBinUser foxBinUser, AccessToken accessToken) throws UniqueViolationException {
-        foxBinUser.getAccessTokens().add(accessToken);
+    public void addAccessToken(FoxBinUser foxBinUser, AccessToken accessToken) {
+        ObjectBox.get().runInTx(() -> {
+            foxBinUser.getAccessTokens().add(accessToken);
 
-        DBUtils
-                .boxForUsers()
-                .put(foxBinUser);
+            DBUtils
+                    .boxForUsers()
+                    .put(foxBinUser);
+        });
     }
 
-    public void removeAccessToken(AccessToken accessToken) {
-        DBUtils
-                .boxForTokens()
-                .remove(accessToken);
-    }
+    public FoxBinUser createFoxBinUser(String username, String password) throws Exception {
+        return ObjectBox.get().callInTx(() -> {
+            FoxBinUser foxBinUser = FoxBinUser.create(username, CryptUtils.toMd5(password));
 
-    public FoxBinUser createFoxBinUser(String username, String password) throws UniqueViolationException {
-        FoxBinUser foxBinUser = FoxBinUser.create(username, CryptUtils.toMd5(password));
+            DBUtils
+                    .boxForUsers()
+                    .put(foxBinUser);
 
-        DBUtils
-                .boxForUsers()
-                .put(foxBinUser);
-
-        return foxBinUser;
+            return foxBinUser;
+        });
     }
 
     public void createRootUser() {
-        Box<FoxBinUser> foxBinUserBox = DBUtils.boxForUsers();
-        if (DBUtils.findFirst(
-                foxBinUserBox
-                        .query()
-                        .equal(FoxBinUser_.username, "root", QueryBuilder.StringOrder.CASE_SENSITIVE)
-                        .build()
-        ) != null) return;
+        ObjectBox.get().runInTx(() -> {
+            Box<FoxBinUser> foxBinUserBox = DBUtils.boxForUsers();
+            if (DBUtils.findFirst(
+                    foxBinUserBox
+                            .query()
+                            .equal(FoxBinUser_.username, "root", QueryBuilder.StringOrder.CASE_SENSITIVE)
+                            .build()
+            ) != null) return;
 
-        foxBinUserBox.put(FoxBinUser.create("root", "root"));
+            foxBinUserBox.put(FoxBinUser.create("root", "root"));
+        });
     }
 
     @Scheduled(initialDelay = Constants.ONE_MINUTE_MS, fixedRate = Constants.HALF_DAY_MS)
     public void checkTokens() {
-        for (FoxBinUser foxBinUser : getAllUsers()) {
-            for (AccessToken accessToken : foxBinUser.getAccessTokens()) {
-                if (accessToken.getLastUsedAt() + Constants.NINETY_DAYS_MS < System.currentTimeMillis())
-                    removeAccessToken(accessToken);
+        ObjectBox.get().runInTx(() -> {
+            List<AccessToken> tokesToRemove = new ArrayList<>();
+
+            for (FoxBinUser foxBinUser : getAllUsers()) {
+                for (AccessToken accessToken : foxBinUser.getAccessTokens()) {
+                    if (accessToken.getLastUsedAt() + Constants.NINETY_DAYS_MS < System.currentTimeMillis())
+                        tokesToRemove.add(accessToken);
+                }
             }
-        }
+
+            DBUtils
+                    .boxForTokens()
+                    .remove(tokesToRemove);
+        });
     }
 }
